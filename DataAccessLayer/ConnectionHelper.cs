9def92a2d69aac2a43d9150910d829643ee315d4
Blaine Pavlock <146504016+PavlockBlaine03@users.noreply.github.com>
2024-05-12 00:06:27 -0500

    Add files via upload

---
+++ DataAccessLayer/ConnectionHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    static internal class ConnectionHelper
+    {
+        internal static string ConnectionString
+        {
+            get
+            {
+                return ConfigurationManager.ConnectionStrings["CookBookConnectionString"].ConnectionString;
+            }
+        }
+    }
+}
